9501d636ff19d08049b505419049812fdecfe055
haleytoye <hjtoye@gmail.com>
2023-08-25 12:55:55 -0400

    api documentation

--- dictionary-project/src/Dictionary.js
+++ dictionary-project/src/Dictionary.js
@@ -12,6 +12,7 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
+    //documentaion: https://www.shecodes.io/learn/apis/dictionary
     let key = "1o0f4aea9af54436031a3t4f3b2ca21f";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
     axios.get(apiUrl).then(handleResponse);
