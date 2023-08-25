8cdf55839d3c4cd8554f7e1d787bd9853674088a
haleytoye <hjtoye@gmail.com>
2023-08-25 12:54:08 -0400

    Axios working with SheCodes api call

--- dictionary-project/src/Dictionary.js
+++ dictionary-project/src/Dictionary.js
@@ -6,16 +6,14 @@ export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
   function handleResponse(response) {
-    console.log(response);
+    console.log(response.data);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
 
-    let word = "sunrise";
     let key = "1o0f4aea9af54436031a3t4f3b2ca21f";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${key}`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
