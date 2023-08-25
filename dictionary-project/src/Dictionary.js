c6b13ee55cd1670634d296a470802379ed00b2a6
haleytoye <hjtoye@gmail.com>
2023-08-25 17:03:31 -0400

    set up Results & Meanings to display api response

--- dictionary-project/src/Dictionary.js
+++ dictionary-project/src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data);
+    setResults(response.data);
   }
 
   function search(event) {
@@ -19,7 +22,6 @@ export default function Dictionary() {
   }
 
   function handleKeywordChange(event) {
-    console.log(event.target.value);
     setKeyword(event.target.value);
   }
 
@@ -28,6 +30,8 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
+//component  name - property name - value name <Results results = {results}/>
